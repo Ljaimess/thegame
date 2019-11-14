fe5db05aae247ef9a09f68766743929ace2c2532
johnericward <johnericward@gmail.com>
2019-11-13 20:51:02 -0500

    added p1 stats to player reducer and monster1 stats to map reducer

--- client/src/features/map/reducer.js
+++ client/src/features/map/reducer.js
@@ -1,6 +1,18 @@
 
 const initialState = {
     tiles: [],
+
+    name: "monster1",
+    hp: 100,
+    claws: {
+        value: 30,
+        uses: 300,
+    },
+    acidBreath: {
+        value: 50,
+        uses: 30,
+    },
+
 };
 
 const mapReducer = (state = initialState, action) => {
--- client/src/features/player/reducer.js
+++ client/src/features/player/reducer.js
@@ -4,6 +4,25 @@ const initialState = {
     spriteLocation: "0px 0px",
     direction: "east",
     walkIndex: 0,
+    name: "josephCampbell",
+    hp: 300,
+    saber: {
+        value: 15,
+        uses: 200,
+    },
+    blaster: {
+        value: 50,
+        uses: 30,
+    },
+    attackSpell: {
+        value: 100,
+        uses: 5,
+    },
+    healingSpell: {
+        value: 25,
+        uses: 15,
+    },
+
 };
 
 const playerReducer = (state = initialState, action) => {
